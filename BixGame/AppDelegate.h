c4120c470c36ad42dd3db00efb4d94b704d404ba
Nikola Kotarov <bbyte@webhostec.com>
2013-05-03 22:05:17 +0300

    Initial Commit

---
+++ BixGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BixGame
+//
+//  Created by Nikola Kotarov on 5/3/13.
+//  Copyright (c) 2013 Nikola Kotarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
